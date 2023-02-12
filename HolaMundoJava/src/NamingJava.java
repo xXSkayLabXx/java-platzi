86ecfc58b676d90480a5f1b3e6debb0effe52cd8
xXSkayLabXx <fabiandrescuar@gmail.com>
2023-02-12 14:45:58 -0500

    Como llamar variables

---
+++ HolaMundoJava/src/NamingJava.java
@@ -0,0 +1,19 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone = 33337777;
+        int celPhone = 55553333;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName = "Colombia";
+        String _backgroundColor = "blue";
+
+        String currency$ = "COL";
+        String background_color = "Red";
+
+        int POSITION  = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+    }
+}
