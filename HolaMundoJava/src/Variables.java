49f584cbf766abc564889d785648f2d291b3038a
xXSkayLabXx <fabiandrescuar@gmail.com>
2023-02-12 14:13:26 -0500

    agregro infomacion en las variables

--- HolaMundoJava/src/Variables.java
+++ HolaMundoJava/src/Variables.java
@@ -1,2 +1,14 @@
 public class Variables {
+    public static void main(String[] args) {
+        //delcarando una variable
+        int speed;
+        //Asignamos valor a la variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Fabian Andres Cuartas";
+
+        System.out.println(employeeName);
+    }
 }
