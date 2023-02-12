91823439d8d3c478bb71878113c65ce8046383be
xXSkayLabXx <fabiandrescuar@gmail.com>
2023-02-12 18:10:48 -0500

    uso la libreria Math

---
+++ HolaMundoJava/src/MathematicOperations.java
@@ -0,0 +1,32 @@
+public class MathematicOperations{
+    public static void main(String[] args) {
+        double x = 2.1;
+        double y = 3;
+
+        //devuelve un entero hacia arriba
+        System.out.println(Math.ceil(x));
+        //devuelve un entero hacia abajo
+        System.out.println(Math.floor(x));
+        //deveuleve un numero elevado a otro numero
+        System.out.println(Math.pow(x,y));
+        //devuelve el numeor mayor
+        System.out.println(Math.max(x,y));
+        //devuelve la raiz caudrada
+        System.out.println(Math.sqrt(y));
+
+        //area de un circulo
+        //pi * r2
+        System.out.println(Math.PI * Math.pow(y,2));
+
+        //area de una esfera
+        // 4*PI*r2
+        System.out.println(4 * Math.PI * Math.pow(y,2));
+
+        //Volumen de una esfera
+        //(4/3)*pi * r3
+        System.out.println((4/3) * Math.PI * Math.pow(y,3));
+
+
+
+    }
+}
