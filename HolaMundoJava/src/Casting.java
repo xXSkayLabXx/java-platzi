44b1c07f2eb8ebf957f073ed5fec19c6d9ecfd54
xXSkayLabXx <fabiandrescuar@gmail.com>
2023-02-12 19:30:47 -0500

    casteo

---
+++ HolaMundoJava/src/Casting.java
@@ -0,0 +1,17 @@
+public class Casting {
+    public static void main(String[] args) {
+        //en un año ubicar 30 perritos
+        //cuantos perritos ubique al mes
+        double monthlyDogs = 30.0/12.0;
+        System.out.println(monthlyDogs);
+
+        //estimacion
+        int estimatedMonthlyDogs = (int) monthlyDogs;
+        System.out.println(estimatedMonthlyDogs);
+
+        //exactitud
+        int a = 30;
+        int b = 12;
+        System.out.println((double) a/b);
+    }
+}
